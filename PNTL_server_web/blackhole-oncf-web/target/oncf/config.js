9e26b96067f9040f15141ae664b61cd6c602e65f
yuanzhou <yuanzhou.yuanzhou@huawei.com>
2017-05-18 16:38:29 +0800

    delete all class and jar file in target

--- PNTL_server_web/blackhole-oncf-web/target/oncf/config.js
+++
@@ -1,8 +0,0 @@
-var configData = {
-    "company":"noSSO",
-    "fip_disable": true, /*display fip function tab*/
-    "sso_ip_port":"192.168.0.0:8443",
-    "server_ip_port":"192.168.0.0:7443"
-};
-
-window.configData = configData || {};
