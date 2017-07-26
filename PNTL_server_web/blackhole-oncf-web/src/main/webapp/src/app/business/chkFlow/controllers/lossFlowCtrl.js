49f340a8dbd69530992aa739f0dde9e20f15f36c
xuemaosai <xuemaosai@huawei.com>
2017-07-26 17:27:16 +0800

    时延丢包问题修改

--- PNTL_server_web/blackhole-oncf-web/src/main/webapp/src/app/business/chkFlow/controllers/lossFlowCtrl.js
+++ PNTL_server_web/blackhole-oncf-web/src/main/webapp/src/app/business/chkFlow/controllers/lossFlowCtrl.js
@@ -203,6 +203,7 @@ define(["language/chkFlow",
                     var lossInfoPromise = lossFlowServ.getLossInfo();
                     lossInfoPromise.then(function(responseData){
                         getLossLinkInfo(responseData);
+                        $("#packets_loss_chart").html("");
                         loss_info_chart();
                     },function(responseData){
                         //showERRORMsg
@@ -226,7 +227,7 @@ define(["language/chkFlow",
                     getIpList(para);
                 };
                 init();
-                var autoRefresh = $interval(getIpList, 30000);
+                var autoRefresh = $interval(getIpList, 60000);
                 $scope.stopAutoRefresh = function () {
                     if (autoRefresh) {
                         $interval.cancel(autoRefresh);
